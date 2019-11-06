b6905a93c787d64e555ce7d461f1ffae51973642
Ferrisx4 <ferrisx4@gmail.com>
2019-11-06 13:42:27 -0500

    Replaced this with other script

--- scripts/drop_indexes.sql
+++
@@ -1,4 +0,0 @@
-set search_path=public,so,frange,genetic_code,chado;
-
-select 'drop index ' || schemaname || '.' || indexname || ';'
-from pg_indexes where indexname like '%idx%' AND schemaname = 'chado';
