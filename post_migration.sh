be482a0aa568c9dfadfb776345668c110b3383b0
Ferrisx4 <ferrisx4@gmail.com>
2020-11-25 14:06:52 -0500

    Get Tripal HQ and Eutils modules

--- post_migration.sh
+++ post_migration.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Populate the analysis_organism table (prior to tripal_manage_analysis)
 drush scr scripts/populate-analysis_organism.php
 
@@ -8,3 +10,17 @@ drush pm-enable tripal_manage_analyses
 drush scr scripts/migrate_node_fields.php
 drush scr scripts/convert_node_links_to_chado.php
 drush scr scripts/convert_sourceuris_from_nodes.php
+
+# Get the Tripal HQ and Tripal EUtils modules, move them to the parent directory, then enable them.
+git clone https://github.com/NAL-i5K/tripal_eutils.git
+git clone https://github.com/statonlab/tripal_hq.git
+# Also grab the field_permissions module which is recommended for use with HQ.
+drush pm-download field_permissions
+
+mv tripal_eutils ..
+mv tripal_hq ..
+
+drush pm-enable tripal_eutils -y
+drush pm-enable tripal_hq tripal_hq_imports tripal_hq_permissions -y
+# Also field_permissions
+drush pm-enable field_permissions -y
